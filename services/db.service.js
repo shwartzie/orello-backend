337b26ebd1208281d4b539e7c4953c1345824a87
Shaul Lavochkin <shaul919191@gmail.com>
2022-07-26 23:45:01 +0300

    added dev DB

--- services/db.service.js
+++ services/db.service.js
@@ -3,7 +3,7 @@ const MongoClient = require('mongodb').MongoClient
 const config = require('../config')
 
 module.exports = {
-    getCollection
+	getCollection
 }
 
 // Database Name
@@ -12,29 +12,29 @@ const dbName = 'orello_db'
 var dbConn = null
 
 async function getCollection(collectionName) {
-    try {
-        const db = await connect()
-        const collection = await db.collection(collectionName)
-        return collection
-    } catch (err) {
-        logger.error('Failed to get Mongo collection', err)
-        throw err
-    }
+	try {
+		const db = await connect()
+		const collection = await db.collection(collectionName)
+		console.log(collection)
+		return collection
+	} catch (err) {
+		logger.error('Failed to get Mongo collection', err)
+		throw err
+	}
 }
 
 async function connect() {
-    if (dbConn) return dbConn
-    try {
-        const client = await MongoClient.connect(config.dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
-        const db = client.db(dbName)
-        dbConn = db
-        return db
-    } catch (err) {
-        logger.error('Cannot Connect to DB', err)
-        throw err
-    }
+	if (dbConn) return dbConn
+	try {
+		const client = await MongoClient.connect(config.dbURL, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		})
+		const db = client.db(dbName)
+		dbConn = db
+		return db
+	} catch (err) {
+		logger.error('Cannot Connect to DB', err)
+		throw err
+	}
 }
-
-
-
-
