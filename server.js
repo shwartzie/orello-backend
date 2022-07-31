89c60b4d59769fd03a0e5d120e7c9531ee1ab994
Shaul Lavochkin <shaul919191@gmail.com>
2022-07-31 09:38:37 +0300

    heroku build

--- server.js
+++ server.js
@@ -46,9 +46,9 @@ setupSocketAPI(http)
 // Make every server-side-route to match the index.html
 // so when requesting http://localhost:3030/index.html/car/123 it will still respond with
 // our SPA (single page app) (the index.html file) and allow vue/react-router to take it from there
-// app.get('/**', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// })
+app.get('/**', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 const logger = require('./services/logger.service')
 const port = process.env.PORT || 3030
